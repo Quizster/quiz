16307c02e3ad750e22cc4eaeceec66055433d4f9
Tony Batty <tonyeb87@gmail.com>
2018-10-23 11:40:10 +0100

    first commit

---
+++ jest.config.js
@@ -0,0 +1,10 @@
+// jest.config.js
+module.exports = {
+    "testURL": "http://localhost/",
+    setupFiles: [
+      '<rootDir>/tests/setup.js'
+    ],
+    "moduleNameMapper": {
+      "^.+\\.(css|less|scss)$": "identity-obj-proxy"
+    }
+  };
---
+++ src/components/App.js
@@ -0,0 +1,14 @@
+import React from "react";
+import "../styles/App.scss";
+
+class App extends React.Component {
+  constructor() {
+    super();
+  }
+
+  render() {
+    return <div>Hello World</div>;
+  }
+}
+
+export default App;
