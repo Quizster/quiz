6d7896460077718efa787770689c7918e47333f4
ljsikuade <40580944+ljsikuade@users.noreply.github.com>
2018-10-23 13:02:50 +0100

    Basics done for client host rendering

--- src/components/App.js
+++ src/components/App.js
@@ -1,13 +1,39 @@
 import React from "react";
 import "../styles/App.scss";
+import Start from "./Start";
+import HostQuiz from "./HostQuiz";
+import ClientQuiz from "./ClientQuiz";
 
 class App extends React.Component {
   constructor() {
     super();
+    this.state = { quizStart: true, hostQuiz: false, clientQuiz: false };
+    this.handleClientHostDecision = this.handleClientHostDecision.bind(this);
+  }
+  //Which h1 did the click on? Conditionally render the components through state.
+  handleClientHostDecision(decision) {
+    //The 'Start' component will always need to be removed.
+    this.setState({ quizStart: false });
+    //Must be either host or client for this function to be called.
+    decision === "host"
+      ? this.setState({ hostQuiz: true })
+      : this.setState({ clientQuiz: true });
+  }
+
+  componentDidMount() {
+    //fetch the questions. Store them in state.
   }
 
   render() {
-    return <div>Hello World</div>;
+    return (
+      <main>
+        {this.state.quizStart && (
+          <Start handleClientHostDecision={this.handleClientHostDecision} />
+        )}
+        {this.state.hostQuiz && <HostQuiz />}
+        {this.state.clientQuiz && <ClientQuiz />}
+      </main>
+    );
   }
 }
 
