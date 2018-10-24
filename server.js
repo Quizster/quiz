f4c333028fa046cd1b5b758bacf8f830f0c16bf8
Mariusz Sygnowski <sygnowski.m@gmail.com>
2018-10-24 08:56:57 +0100

    server returning questions, database with questions contains 8 from 20 questions

--- server.js
+++ server.js
@@ -22,6 +22,34 @@ app.get("/", function(req, res) {
   res.render("index");
 });
 
+app.get('/api/questions', function (req, res) {
+  db.any('SELECT question.id AS questionId, answer.id AS answerId, question.question, answer.answer,answer.is_correct FROM answer, question WHERE question.id = answer.question_id')
+    .then(output => {
+      let objectWitQuestionToBeReturned = {};
+      let fullObjectOfanswers = {};
+
+      output.forEach((item, index) => {
+        fullObjectOfanswers[item.answerid] = item.answer;
+        objectWitQuestionToBeReturned[item.questionid] = {
+          id: item.questionid,
+          question: item.question,
+          answers: {
+            [index-2]: fullObjectOfanswers[index-2],
+            [index-1]: fullObjectOfanswers[index-1],
+            [index]: fullObjectOfanswers[index],
+            [index+1]: fullObjectOfanswers[index+1]
+          }
+        }
+      });
+      output.forEach(item => {
+        if (item.is_correct === true) {
+          objectWitQuestionToBeReturned[item.questionid] = Object.assign({}, objectWitQuestionToBeReturned[item.questionid], {correctAnswer: item.answerid} )
+        }
+      })
+            
+      res.json(objectWitQuestionToBeReturned);
+    })
+  })
 app.listen(8080, function() {
   console.log("Listening on port 8080");
-});
+})
