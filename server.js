38fe74a9822efe4c130b92286ee6e1efa8b40466
nicola gibson <nicola.k.gibson@gmail.com>
2018-10-26 14:26:10 +0100

    set port from ev var

--- server.js
+++ server.js
@@ -11,10 +11,6 @@ app.use(bodyParser.json());
 app.use("/static", express.static("static"));
 app.set("view engine", "hbs");
 
-server.listen(4000, function() {
-  console.log("!Port 4000!");
-});
-
 // store player answers in format quizId: { playerId: [answer1, answer2] }
 let playerAnswers = {};
 let connectedPlayers = [];
@@ -64,7 +60,7 @@ io.on("connection", socket => {
 
 const pgp = require("pg-promise")();
 const db = pgp({
-  host: "localhost",
+  host: process.env.DB_HOST,
   port: 5432,
   database: process.env.DB_NAME,
   user: process.env.DB_USERNAME,
---
+++ src/components/SingleResult.js
@@ -0,0 +1,12 @@
+import React from "react";
+
+class SingleResult extends React.Component {
+  constructor(props) {
+    super(props);
+  }
+
+  render() {
+    return <div className="status_display" />;
+  }
+}
+export default SingleResult;
